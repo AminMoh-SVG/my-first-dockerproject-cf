ad955756a6fb32a7e028f8e5aa78d45c93571ebe
AminMoh-SVG <mohamad.oc.amin@gmail.com>
2025-02-25 18:37:39 +0700

    Added Terraform configuration and GitHub Actions workflow

---
+++ terraform/main.tf
@@ -0,0 +1,44 @@
+provider "aws" {
+  region = "ap-southeast-2"
+}
+
+resource "aws_ecs_cluster" "my_cluster" {
+  name = "my-cluster"
+}
+
+resource "aws_ecs_task_definition" "my_task" {
+  family                   = "my-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  cpu                      = "256"
+  memory                   = "512"
+
+  container_definitions = jsonencode([
+    {
+      name      = "my-container"
+      image     = "amindockrs1311/my-first-dockerproject:latest"
+      cpu       = 256
+      memory    = 512
+      essential = true
+      portMappings = [
+        {
+          containerPort = 3000
+          hostPort      = 3000
+        }
+      ]
+    }
+  ])
+}
+
+resource "aws_ecs_service" "my_service" {
+  name            = "my-service"
+  cluster         = aws_ecs_cluster.my_cluster.id
+  task_definition = aws_ecs_task_definition.my_task.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+  network_configuration {
+    subnets         = ["subnet-12345678"]
+    assign_public_ip = true
+  }
+}
