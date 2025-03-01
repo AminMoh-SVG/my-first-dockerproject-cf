156ff0fb1d37c4b27de74af2b5696a7f93086bc3
AminMoh-SVG <mohamad.oc.amin@gmail.com>
2025-03-01 16:48:36 +0700

    Initial commit:my-first-dockerproject-cf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,44 +1,90 @@
 provider "aws" {
-  region = "ap-southeast-2"
+  region = "ap-southeast-2"  # Updated AWS region
 }
 
-resource "aws_ecs_cluster" "my_cluster" {
+resource "aws_vpc" "main_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "ap-southeast-2a"
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main_vpc.id
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+}
+
+resource "aws_route" "default_route" {
+  route_table_id         = aws_route_table.public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.gw.id
+}
+
+resource "aws_security_group" "ecs_sg" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_ecs_cluster" "ecs_cluster" {
   name = "my-cluster"
 }
 
-resource "aws_ecs_task_definition" "my_task" {
-  family                   = "my-task"
+resource "aws_ecs_task_definition" "ecs_task" {
+  family                   = "my-task-definition"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
-  cpu                      = "256"
-  memory                   = "512"
+  execution_role_arn       = "arn:aws:iam::307946647513:role/ecsTaskExecutionRole"
+  cpu                      = "2048"
+  memory                   = "8182"
 
   container_definitions = jsonencode([
     {
-      name      = "my-container"
-      image     = "amindockrs1311/my-first-dockerproject:latest"
-      cpu       = 256
-      memory    = 512
+      name  = "my-container"
+      image = "307946647513.dkr.ecr.ap-southeast-2.amazonaws.com/my-first-dockerproject-cf:latest"
+      cpu   = 2048
+      memory = 8182
       essential = true
       portMappings = [
         {
-          containerPort = 3000
-          hostPort      = 3000
+          containerPort = 80
+          hostPort      = 80
         }
       ]
     }
   ])
 }
 
-resource "aws_ecs_service" "my_service" {
-  name            = "my-service"
-  cluster         = aws_ecs_cluster.my_cluster.id
-  task_definition = aws_ecs_task_definition.my_task.arn
-  desired_count   = 1
+resource "aws_ecs_service" "ecs_service" {
+  name            = "my-ecs-service"
+  cluster         = aws_ecs_cluster.ecs_cluster.id
+  task_definition = aws_ecs_task_definition.ecs_task.arn
   launch_type     = "FARGATE"
+  desired_count   = 1
+
   network_configuration {
-    subnets         = ["subnet-12345678"]
+    subnets          = [aws_subnet.public_subnet.id]
+    security_groups  = [aws_security_group.ecs_sg.id]
     assign_public_ip = true
   }
 }
+
